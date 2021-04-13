04655baf7dbc15469994f7d8779e96abd62b1899
thecoderansh <76258786+thecoderansh@users.noreply.github.com>
2021-04-13 21:51:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+
+	Engine.run(engine);
+	var option{
+		isStatic:false,
+		restitution:0.3,
+		friction:0:1,
+		density:1.2
+	}
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+
+  groundObject.display();
+  dustbinObj.display();
+
+}
+
+function keyPressed() {
+if(keyCode === UP_arrow) {
+Matter.body.applyForce{paperObject.body,paperObject.body,position,{x:130,y:-145}};
+}
+
+
+
+
+
+
+}
